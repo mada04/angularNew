45c2eb598f520527056309673e4bde78f4fcb0b5
mada04 <madalina.comanescu@alten.com>
2025-01-23 15:16:47 +0200

    detail-filter

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component,inject } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
 import { CommonModule } from '@angular/common';
 import { HousingService } from '../housing.service';
@@ -13,10 +13,19 @@ import { HousingLocation } from '../housinglocation';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  housingLocationList:HousingLocation[]=[];
-  housingService:HousingService=inject(HousingService);
+  housingLocationList: HousingLocation[] = [];
+  housingService: HousingService = inject(HousingService);
+  filteredLocationList: HousingLocation[] = [];
+  constructor() {
+    this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.filteredLocationList=this.housingLocationList;
+  }
 
-  constructor(){
-    this.housingLocationList=this.housingService.getAllHousingLocations();
+  filterResults(text:string){
+    if(!text){
+      this.filteredLocationList=this.housingLocationList;
+      return;
+    }
+    this.filteredLocationList=this.housingLocationList.filter(housingLocation=>housingLocation?.city.toLowerCase().includes(text.toLowerCase()));
   }
 }
