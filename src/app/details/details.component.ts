2dc026401ca1acf1a5e9bd30d5f9ac93f4877fef
mada04 <madalina.comanescu@alten.com>
2025-01-27 09:43:31 +0200

    server json

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -26,8 +26,12 @@ lastName:new FormControl(''),
 email:new FormControl('')
 })
 constructor(){
-  const housingLocationId=Number(this.route.snapshot.params['id'])
-  this.housingLocation=this.housingService.getHousingLocationById(housingLocationId)
+  // const housingLocationId=Number(this.route.snapshot.params['id'])
+  // this.housingLocation=this.housingService.getHousingLocationById(housingLocationId)
+  const housingLocationId=parseInt(this.route.snapshot.params['id'],10);
+  this.housingService.getHousingLocationById(housingLocationId).then(housingLocation=>{
+    this.housingLocation=this.housingLocation
+  })
 }
 submitApplication(){
   this.housingService.submitApplication(
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,12 +13,17 @@ import { HousingLocation } from '../housinglocation';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
   housingLocationList: HousingLocation[] = [];
   housingService: HousingService = inject(HousingService);
   filteredLocationList: HousingLocation[] = [];
+
   constructor() {
-    this.housingLocationList = this.housingService.getAllHousingLocations();
-    this.filteredLocationList=this.housingLocationList;
+    this.housingService.getAllHousingLocations().then((housingLocationList:HousingLocation[])=>{
+
+      this.housingLocationList = housingLocationList
+      this.filteredLocationList=housingLocationList;
+    })
   }
 
   filterResults(text:string){
@@ -26,6 +31,7 @@ export class HomeComponent {
       this.filteredLocationList=this.housingLocationList;
       return;
     }
-    this.filteredLocationList=this.housingLocationList.filter(housingLocation=>housingLocation?.city.toLowerCase().includes(text.toLowerCase()));
+    this.filteredLocationList=this.housingLocationList.filter(
+      housingLocation=>housingLocation?.city.toLowerCase().includes(text.toLowerCase()));
   }
 }
